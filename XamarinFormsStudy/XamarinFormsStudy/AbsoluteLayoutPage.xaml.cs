06da2c18bd1b646d7a0627c3998ccb11c7c97ed2
강준 <junijuniya@naver.com>
2017-07-05 14:36:23 +0900

    초기 커밋.

---
+++ XamarinFormsStudy/XamarinFormsStudy/AbsoluteLayoutPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsStudy
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AbsoluteLayoutPage : ContentPage
+    {
+        public AbsoluteLayoutPage()
+        {
+            InitializeComponent();
+
+        //    //var emveddedImage = new Image { Aspect = Aspect.AspectFit };
+        //    Image image1 = new Image() { Source = ImageSource.FromResource("XamarinFormsStudy.bottom.png") };
+
+        //    Image image2 = new Image() { Source = ImageSource.FromResource("XamarinFormsStudy.bottom.png") };
+
+        //    Image image3 = new Image() { Source = ImageSource.FromResource("XamarinFormsStudy.bottom.png") };
+
+
+        //    AbsoluteLayout.SetLayoutFlags(bottomRightLabel,AbsoluteLayoutFlags.PositionProportional);
+
+        //    this.Content = new AbsoluteLayout()
+        //    {
+        //        Children =
+        //        {
+        //            AbsoluteLayout.layoutfl AbsoluteLayoutFlags.PositionProportional
+                    
+        //        }
+            
+        //    };
+
+        }
+    }
+}
---
+++ XamarinFormsStudy/XamarinFormsStudy/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsStudy
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //Content = new Label
+            //{
+            //    Text = "Hello, Forms!",
+            //    VerticalOptions = LayoutOptions.CenterAndExpand,
+            //    HorizontalOptions = LayoutOptions.CenterAndExpand,
+            //};
+
+            //Content = new StackLayout
+            //{
+            //    VerticalOptions = LayoutOptions.CenterAndExpand,
+            //    Children =
+            //    {
+            //        new Label{
+            //                    Text = "Hello, Forms!",
+            //                    VerticalOptions = LayoutOptions.CenterAndExpand,
+            //                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+            //        }
+            //    }
+            //};
+        }
+    }
+}
