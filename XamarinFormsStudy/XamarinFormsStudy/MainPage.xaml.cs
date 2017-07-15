0beb52f731136c7c75f731caf92baf73b9c33107
강준 <junijuniya@naver.com>
2017-07-16 03:43:50 +0900

    ...........

--- XamarinFormsStudy/XamarinFormsStudy/MainPage.xaml.cs
+++ XamarinFormsStudy/XamarinFormsStudy/MainPage.xaml.cs
@@ -9,6 +9,9 @@ namespace XamarinFormsStudy
 {
     public partial class MainPage : ContentPage
     {
+
+        Dictionary<string, Page> dic = new Dictionary<string, Page>();
+
         public MainPage()
         {
             InitializeComponent();
@@ -32,36 +35,38 @@ namespace XamarinFormsStudy
             //    }
             //};
 
-            Button gridPageButton = new Button() { Text = "GridPage" };
-            gridPageButton.Clicked += Button_Clicked;
+            StackLayout stackLayout = new StackLayout() { VerticalOptions = LayoutOptions.CenterAndExpand };
 
-            Button masterDetailPageButton = new Button() { Text = "MasterDetailPage" };
-            masterDetailPageButton.Clicked += Button_Clicked;
+            dic.Add("MasterDetailPage", new MasterDetailPage1());
+            dic.Add("StackLayoutPage", new StackLayoutPage());
+            dic.Add("AbsoluteLayoutPage", new AbsoluteLayoutPage());
+            dic.Add("RelativeLayoutPage", new RelativeLayoutPage());
+            dic.Add("GridPage", new GridPage());
+            dic.Add("BaseControlPage", new BaseControlPage());
+            dic.Add("WebPickerPage", new WebPickerPage());
+            dic.Add("ListViewPage", new ListViewPage());
+            dic.Add("OxyPlotPage", new OxyPlotPage());
+            dic.Add("OxyPlotLineChartPage", new OxyPlotLineChartPage());
+            dic.Add("SfDataGridPage", new SfDataGridPage());
+            dic.Add("SQLiteSamplePage", new SQLiteSamplePage().GetSampleContentPage());
 
-            Content = new StackLayout
+            foreach (var data in dic)
             {
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                Children =
-                {
-                    gridPageButton,
-                    masterDetailPageButton
-                }
-            };
+                Button button = new Button() { Text = data.Key };
+                button.Clicked += Button_Clicked;
+                stackLayout.Children.Add(button);
+            }
+            var scroll = new ScrollView();
+            scroll.Content = stackLayout;
+            Content = scroll;
+            
 
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
             string pagename = ((Button)sender).Text;
-            switch (pagename)
-            {
-                case "GridPage":
-                    await Navigation.PushAsync(new GridPage());
-                    break;
-                case "MasterDetailPage":
-                    await Navigation.PushAsync(new MasterDetailTestPage());
-                    break;
-            }
+            await Navigation.PushAsync(dic[pagename]);
         }
     }
 }
